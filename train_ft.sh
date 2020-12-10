658f78a9a0ce1b5b84a88da991cdc4c43d2d5da2
Clara Tomasini <794524@unizar.es>
2020-12-10 16:10:39 +0100

    update train script

--- train_ft.sh
+++ train_ft.sh
@@ -3,7 +3,7 @@ n_epochs=20
 
 for i in 0 1 2
 do
-    num_epochs_1=$(($n_epochs+50+60*$i))
+    num_epochs_1=$(($n_epochs+40+80*$i))
     python train_ft_exec.py \
         --model_path data/models/linknet_binary_20/model_0.pt \
         --model_type LinkNet34 \
@@ -20,7 +20,7 @@ do
         --num_classes 1 \
         --train_size 0.6 
 
-    num_epochs_2=$(($n_epochs+60+60*$i))
+    num_epochs_2=$(($n_epochs+80+80*$i))
     python train_ft_exec.py \
         --model_path data/models/linknet_binary_20/model_0.pt \
         --model_type LinkNet34 \
