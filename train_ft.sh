2546d1d9f4d60ea7fe8c494a9ebf5e7f89ba0061
Clara Tomasini <794524@unizar.es>
2020-12-16 11:55:04 +0100

    updated train script

--- train_ft.sh
+++ train_ft.sh
@@ -5,9 +5,9 @@ for i in 0 1 2
 do
     num_epochs_1=$(($n_epochs+25+50*$i))
     python train_ft_exec.py \
-        --model_path data/models/linknet_binary_20/model_0.pt \
-        --model_type LinkNet34 \
-        --model_name linknet \
+        --model_path data/models/unet16_binary_20/model_0.pt \
+        --model_type UNet16 \
+        --model_name unet16 \
         --num_mod 0 \
         --type_mod ent \
         --jaccard-weight 0.3 \
@@ -22,9 +22,9 @@ do
 
     num_epochs_2=$(($n_epochs+50+50*$i))
     python train_ft_exec.py \
-        --model_path data/models/linknet_binary_20/model_0.pt \
-        --model_type LinkNet34 \
-        --model_name linknet \
+        --model_path data/models/unet16_binary_20/model_0.pt \
+        --model_type UNet16 \
+        --model_name unet16 \
         --num_mod 0 \
         --type_mod ent \
         --jaccard-weight 0.3 \
