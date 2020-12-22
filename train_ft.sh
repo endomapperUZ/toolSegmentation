09e62b1b3a80fdc1a80c85b644f7c1dc75ca47c6
Clara Tomasini <794524@unizar.es>
2020-12-22 11:34:30 +0100

    updated train script

--- train_ft.sh
+++ train_ft.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 n_epochs=20
 
-
-num_epochs_1=$(($n_epochs+25+50*$i))
 python train_ft_exec.py \
     --model_path data/models/linknet_binary_20/model_0.pt \
     --model_type LinkNet34 \
@@ -13,13 +11,12 @@ python train_ft_exec.py \
     --fold 0 \
     --root /workspace/toolSegmentation \
     --batch-size 8 \
-    --n-epochs $num_epochs_1 \
+    --n-epochs 40 \
     --lr 0.0001 \
     --workers 3 \
     --num_classes 1 \
     --train_size 0.84426 
 
-num_epochs_2=$(($n_epochs+50+50*$i))
 python train_ft_exec.py \
     --model_path data/models/linknet_binary_20/model_0.pt \
     --model_type LinkNet34 \
@@ -30,7 +27,7 @@ python train_ft_exec.py \
     --fold 0 \
     --root /workspace/toolSegmentation \
     --batch-size 8 \
-    --n-epochs $num_epochs_2 \
+    --n-epochs 60 \
     --lr 0.00001 \
     --workers 3 \
     --num_classes 1 \
