2d208e899d74b6dcd58c040d13bb98c6a970531e
Clara Tomasini <794524@unizar.es>
2020-12-25 09:55:39 +0100

    updated train

--- prepare_train_val_ft.py
+++ prepare_train_val_ft.py
@@ -4,23 +4,20 @@ import random
 def get_split(root, fold, train_size ):
     
   train_path = glob.glob(root + "/data/" + fold.replace('raw','masks') + "/*.png") 
-  train_file_names = []
-  val_file_names = []
+  #train_file_names = []
+  #val_file_names = []
   num_files = len(train_path)
   print(num_files)
-  file_ind = [k for k in range(num_files)]
-  random.shuffle(file_ind)
-  for i in range(int(num_files*train_size)):
-    train_file_names.append(train_path[file_ind[i]])
-  for m in range(int(num_files*train_size), num_files):
-    val_file_names.append(train_path[file_ind[m]])
-  random.shuffle(train_file_names)
-  print(val_file_names)
-  random.shuffle(val_file_names)
-  print(val_file_names)
-  #train_file_names = train_path[file_ind[:int(num_files*train_size)]]
-  #val_file_names = train_path[file_ind[int(num_files*train_size):]]
-
+  #file_ind = [k for k in range(num_files)]
+  #random.shuffle(file_ind)
+  #for i in range(int(num_files*train_size)):
+    #train_file_names.append(train_path[file_ind[i]])
+  #for m in range(int(num_files*train_size), num_files):
+    #val_file_names.append(train_path[file_ind[m]])
+  train_file_names = train_path[:int(num_files*train_size)]
+  val_file_names = train_path[int(num_files*train_size):]
+  print(len(train_file_names))
+  print(len(val_file_names))
   #file_name = "train_val_dataset"+fold.replace('train_data/','').replace('_raw','')+".txt"
   #f = open(file_name, "a")
   #f.write("Train files : \n")
