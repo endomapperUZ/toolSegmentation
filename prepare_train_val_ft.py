e0750def33b3d6336db36b0038cbd027faa1fe24
Clara Tomasini <794524@unizar.es>
2020-12-25 09:50:18 +0100

    updated train

--- prepare_train_val_ft.py
+++ prepare_train_val_ft.py
@@ -14,14 +14,18 @@ def get_split(root, fold, train_size ):
     train_file_names.append(train_path[file_ind[i]])
   for m in range(int(num_files*train_size), num_files):
     val_file_names.append(train_path[file_ind[m]])
+  random.shuffle(train_file_names)
+  print(val_file_names)
+  random.shuffle(val_file_names)
+  print(val_file_names)
   #train_file_names = train_path[file_ind[:int(num_files*train_size)]]
   #val_file_names = train_path[file_ind[int(num_files*train_size):]]
 
-  file_name = "train_val_dataset"+fold.replace('train_data/','').replace('_raw','')+".txt"
-  f = open(file_name, "a")
-  f.write("Train files : \n")
-  f.write("\n".join(train_file_names))
-  f.write("\n Val files : \n")
-  f.write("\n".join(val_file_names))
-  f.close()
+  #file_name = "train_val_dataset"+fold.replace('train_data/','').replace('_raw','')+".txt"
+  #f = open(file_name, "a")
+  #f.write("Train files : \n")
+  #f.write("\n".join(train_file_names))
+  #f.write("\n Val files : \n")
+  #f.write("\n".join(val_file_names))
+  #f.close()
   return train_file_names, val_file_names
