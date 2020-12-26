0154d7accd8319e79ebec3bfdd9bf81e94475aae
Clara Tomasini <794524@unizar.es>
2020-12-26 17:54:20 +0100

    updated train

--- train_ft.sh
+++ train_ft.sh
@@ -7,11 +7,11 @@ python train_ft_exec.py \
     --model_name linknet \
     --num_mod 0 \
     --type_mod ent \
-    --jaccard-weight 0.3 \
+    --jaccard-weight 0.5 \
     --fold 0 \
     --root /home/ropert/ctomasin/storage/toolSegmentation \
     --batch-size 8 \
-    --n-epochs 40 \
+    --n-epochs 25 \
     --lr 0.001 \
     --workers 3 \
     --num_classes 1 \
@@ -23,11 +23,11 @@ python train_ft_exec.py \
     --model_name linknet \
     --num_mod 0 \
     --type_mod ent \
-    --jaccard-weight 0.3 \
+    --jaccard-weight 0.5 \
     --fold 0 \
     --root /home/ropert/ctomasin/storage/toolSegmentation \
     --batch-size 8 \
-    --n-epochs 60 \
+    --n-epochs 30 \
     --lr 0.0001 \
     --workers 3 \
     --num_classes 1 \
