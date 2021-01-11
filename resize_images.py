68e2d46c5860530624141d744858f3305d4b7dc8
Clara Tomasini <794524@unizar.es>
2021-01-11 22:30:15 +0100

    updated train script

--- resize_images.py
+++ resize_images.py
@@ -7,7 +7,7 @@ import glob
 import os
 
 def main():
-  files_train = glob.glob('/workspace/ctomasin/toolSegmentation/data/train_data/train_masks/video39*.png')
+  files_train = glob.glob('/workspace/ctomasin/toolSegmentation/data/train_data/train_masks/*.png')
   files_train.sort()
   print(len(files_train))
   for i in range(3168):
@@ -33,5 +33,27 @@ def main():
     else :     
       print('No resize necessary')
 
+  for i in range(3168,14267):
+    mask_file_name6 = files_train[i]
+    img_file_name6 = mask_file_name6.replace('masks','raw').replace('mask','frame')
+    image = load_image(mask_file_name6)
+    mask = load_mask(mask_file_name6,'binary')
+    print(i)
+    print('shape image before resize : '+ str(image.shape))
+    if (image.shape==(1080, 1920, 3)):
+      image = image[24:, 640:,:]
+      print('shape image after resize : '+ str(image.shape))
+
+      print('shape mask : '+ str(mask.shape))
+
+      image = image[:,:,::-1]
+      img_file_name6 = mask_file_name6.replace('masks','raw').replace('mask','frame')
+      print(img_file_name6)
+      os.remove(img_file_name6)
+      os.remove(mask_file_name6)
+      cv2.imwrite(mask_file_name6, mask)
+      cv2.imwrite(img_file_name6, image)  
+    else :
+      print('No resize necessary')
 if __name__ == '__main__':
     main()
