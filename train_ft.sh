b6ba29ba7acb3da0f721525db175c6e7e9155f00
Clara Tomasini <794524@unizar.es>
2021-01-13 09:57:07 +0100

    updated train script

--- train_ft.sh
+++ train_ft.sh
@@ -33,3 +33,35 @@ python train_ft_exec.py \
     --num_classes 1 \
     --train_size 0.84426
 
+python train_ft_exec.py \
+    --model_path data/models/linknet_binary_20/model_0.pt \
+    --model_type LinkNet34 \
+    --model_name linknet \
+    --num_mod 0 \
+    --type_mod ent \
+    --jaccard-weight 0.6 \
+    --fold 0 \
+    --root /workspace/ctomasin/toolSegmentation \
+    --batch-size 8 \
+    --n-epochs 50 \
+    --lr 0.0001 \
+    --workers 3 \
+    --num_classes 1 \
+    --train_size 0.84426
+
+python train_ft_exec.py \
+    --model_path data/models/linknet_binary_20/model_0.pt \
+    --model_type LinkNet34 \
+    --model_name linknet \
+    --num_mod 0 \
+    --type_mod ent \
+    --jaccard-weight 0.6 \
+    --fold 0 \
+    --root /workspace/ctomasin/toolSegmentation \
+    --batch-size 8 \
+    --n-epochs 60 \
+    --lr 0.00001 \
+    --workers 3 \
+    --num_classes 1 \
+    --train_size 0.84426
+
